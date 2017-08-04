5c4a8f22ec206e3990181b124a663eef1843d087
Esha Randhawa <esha.roda@gmail.com>
2017-08-03 19:10:11 -0700

    Update string.js

--- string.js
+++ string.js
@@ -1,26 +1,20 @@
 var https = require('https');
 
-
 function getAndPrintHTML () {
-
   var requestOptions = {
   host: 'sytantris.github.io',
   path: '/http-examples/step3.html'
 };
-
   https.get(requestOptions, function (response) {
-
   response.setEncoding('utf8');
    var content = "";
     response.on('data', function (data) {
       content += data;
     });
-
     response.on('end', function () {
       console.log(content);
     });
   });
-
 }
 
 getAndPrintHTML();
