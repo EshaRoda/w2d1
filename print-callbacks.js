0185586c19ed66fe002c38805c0f29764a37216a
Esha Randhawa <esha.roda@gmail.com>
2017-08-03 19:11:23 -0700

    Update print-callbacks.js

--- print-callbacks.js
+++ print-callbacks.js
@@ -6,16 +6,12 @@ var requestOptions = {
 };
 
 function getHTML (options, callback) {
-
   https.get(options, function (response) {
-
     response.setEncoding('utf8');
-
     var content = "";
     response.on('data', function (data) {
       content += data;
     });
-
     response.on('end', function () {
       callback(content);
     });
